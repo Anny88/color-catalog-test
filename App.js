1473e3860e6b3570b3ad25be96b50de2b4f22fe1
Anna Klimenko <anna.klimenko@pwc.com>
2021-01-11 16:29:50 +0100

    Added button, alert, CSS style & flex layout

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { Text, View, Alert, Button, StyleSheet } from 'react-native';
+import { Text, View, Alert, Button, StyleSheet, Dimensions, Image } from 'react-native';
+import pic1 from "./assets/Download.jpeg";
+import pic2 from "./assets/images.jpeg";
 
 export default function App() {
   const onButtonPress = () => {
     Alert.alert(new Date().toLocaleTimeString() + " button pressed");
   }
   return (
-    <View style={styles.page}>
-      <Text style={[styles.text, styles.selectedText]}>Red</Text>
-      <Text style={styles.text}>Green</Text>
-      <Text style={styles.text}>Blue</Text>
-        <Button title="Click me!" onPress={onButtonPress} />
+    <View>
+        <View style={styles.page}>
+          <Text style={[styles.text, styles.selectedText]}>Red</Text>
+          <Text style={styles.text}>Green</Text>
+          <Text style={styles.text}>Blue</Text>
+        </View>
+        <Button title="Click me!" onPress={onButtonPress} style={{ marginTop: 40}}/>
+        <View style={styles.imageContainer}>
+            <Image source={pic1} style={styles.image} />
+            <Image source={pic2} style={styles.image} />
+        </View>
     </View>
-  );
+);
 }
 
 const styles = StyleSheet.create({
     page: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems:  "center",
         marginTop: 40,
         backgroundColor: '#f2f2f2'
+    },
+    imageContainer: {
+        marginTop: 40
     },
     text: {
         fontSize: 16,
@@ -34,6 +41,12 @@ const styles = StyleSheet.create({
      selectedText: {
         color: '#000',
         backgroundColor: '#fff',
-     }
+     },
+    image: {
+        flex: 1,
+        borderRadius: 50,
+        margin: 10,
+        width: Dimensions.get("window").width - 10
+    }
 });
 
